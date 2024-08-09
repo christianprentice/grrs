617f007da170df34cf185adb75a90712757631da
Christian <purin213@hotmail.com>
2024-08-10 00:50:26 +0900

    lint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{self, Write};
 use anyhow::{Context, Result};
 use clap::Parser;
 use colored::*;
@@ -12,17 +13,18 @@ struct Cli {
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-
     let pattern = &args.pattern;
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+    let _stdout = io::stdout();
+    let mut handle = _stdout.lock();
 
     for line in content.lines() {
         if line.contains(*&pattern) {
             let colored_line = utils::colorize_substring(&line, &pattern, Color::Green);
-            println!("{}", colored_line);
+            //println!("{}", colored_line);
+            writeln!(handle, "{}", colored_line);
         }
     }
-
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -2,12 +2,9 @@ use colored::*;
 
 pub fn colorize_substring(haystack: &str, needle: &str, color: Color) -> String{
     let start_index = haystack.find(needle).unwrap_or(0);
-
     let before_substring = &haystack[..start_index];
     let after_substring = &haystack[start_index + needle.len()..];
-
     let colored_needle = needle.color(color);
-
     format!(
         "{}{}{}",
         before_substring,
